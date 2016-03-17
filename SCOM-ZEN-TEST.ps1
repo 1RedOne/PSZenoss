3c08c25c679285d9c5c75a959e33c1bc6a8bee00
Stephen Owen <Stephen@foxdeploy.com>
2016-03-17 13:53:10 -0400

    First Code Push

---
+++ SCOM-ZEN-TEST.ps1
@@ -0,0 +1,24 @@
+﻿$user = "SCOMTest"
+$pass = "dCxrwM"
+
+$device = 'cho3w5ap02.corpstage.transunion.com'
+$deviceUrl = "http://chmgl5mn07.corpstage.transunion.com:8080/zport/dmd/Devices/Server/Microsoft/Windows/Devices/devices/$device"
+
+$base64 = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $user, $pass)))
+
+invoke-restmethod $deviceUrl -Headers @{Authorization=("Basic {0}" -f $base64)} 
+
+#experimental, creating a new event
+#$data = @{Device="$device";Summary="TestEvent";Component="Appinfo";Severity='Critical';evclasskey='/Status'} | ConvertTo-Json -Compress
+#$body = @{action="Events";method="add_event";data=$data;tid=1} | ConvertTo-Json -Compress
+#
+#invoke-restmethod "http://chmgl5mn07.corpstage.transunion.com:8080/zport/dmd/Events/" -Headers @{Authorization=("Basic {0}" -f $base64)} -Body $body -Method post
+
+
+#get list of devices
+$data = @{uid='/Server/Linux'} | ConvertTo-Json -Compress
+$body = @{action="DeviceRouter";method="getDevices";data=$data;type='rpc';tid=1} | ConvertTo-Json -Compress
+
+invoke-restmethod "http://chmgl5mn07.corpstage.transunion.com:8080/zport/dmd/device_router/" `
+    -Headers @{Authorization=("Basic {0}" -f $base64)} -Body $body -Method post -ContentType 'application/json' | 
+    tee -variable test
---
+++ get-ZenossDevice.ps1
@@ -0,0 +1,10 @@
+﻿Function Get-ZenossDevice{
+#WORKING GET DEVICES HOLY MOTHER OF HAM
+$data = $NULL| ConvertTo-Json -Compress
+$body = @{action="DeviceRouter";method="getDevices";data=$data;type='rpc';tid='4832'} | ConvertTo-Json -Compress
+
+invoke-restmethod "http://chmgl5mn07.corpstage.transunion.com:8080/zport/dmd/device_router/" `
+    -Headers @{Authorization=("Basic {0}" -f $base64)} -Body $body -Method post -ContentType 'application/json' | 
+    tee -variable test
+
+    $test.result.devices}
